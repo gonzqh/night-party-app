b47a85e97820237e39c4072e0633c49b3dbc35cd
YATIMARKA <gonzalo.qh.94@gmail.com>
2026-05-01 19:39:10 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'night-party-app',
+  webDir: 'www'
+};
+
+export default config;
